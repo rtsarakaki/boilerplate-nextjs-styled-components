3e965e835d6bf1751dd2b7d487a75a639be16edc
rtsarakaki <rtsarakaki@gmail.com>
2024-02-29 21:40:55 -0300

    config: boilerplate finished

--- src/components/main/index.spec.tsx
+++ src/components/main/index.spec.tsx
@@ -1,12 +1,26 @@
 import '@testing-library/jest-dom'
+import 'jest-styled-components'
 import { render, screen } from '@testing-library/react'
 
-import Index from './index'
+import Main from '.'
 
-describe('Index', () => {
-  it('Should render index component', () => {
-    render(<Index />)
-    const heading = screen.getByRole('heading', { name: /Boilerplate NextJS/i })
-    expect(heading).toBeInTheDocument()
+describe('Main', () => {
+  // Renderiza o componente uma vez para todos os testes neste bloco describe
+  beforeEach(() => {
+    render(<Main />)
+  })
+
+  it('should render the heading', () => {
+    expect(
+      screen.getByRole('heading', { name: /Boilerplate/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should not throw any JavaScript errors', () => {
+    const consoleSpy = jest.spyOn(console, 'error')
+
+    expect(consoleSpy).not.toHaveBeenCalled()
+
+    consoleSpy.mockRestore()
   })
 })
--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,7 +1,20 @@
-export default function Index() {
+import * as styles from './styles'
+
+export default function Main() {
   return (
-    <div>
-      <h1>Boilerplate NextJS</h1>
-    </div>
+    <styles.Wrapper>
+      <styles.Logo
+        src="/img/logo.svg"
+        alt="Logo com um átomo na cor branca com fundo rosa ao lado do texto React Avançado"
+      />
+      <styles.Title>Boilerplate</styles.Title>
+      <styles.Description>
+        Typescript, ReactJS, NextJS e Styled Components
+      </styles.Description>
+      <styles.Illustration
+        src="/img/hero-illustration.svg"
+        alt="Imagem de um desenvolvedor de frente para a tela com código"
+      />
+    </styles.Wrapper>
   )
 }
